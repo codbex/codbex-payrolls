5303276a871a718a19ad5eabbcd760e7583b1137
NicoleNG18 <nicole.georgieva18@abv.bg>
2024-10-05 17:22:46 +0300

    add scheduled jobs fro generating payroll entries

---
+++ codbex-payrolls/scheduled-jobs/GeneratePayrolls/GeneratePayrollsService.ts
@@ -0,0 +1,22 @@
+import { insert } from "sdk/db";
+
+export interface Payroll {
+    readonly id: number,
+    readonly employee: number,
+    readonly title: string,
+    readonly netSalary: number,
+    readonly taxes: number,
+    readonly startDate: Date,
+    readonly endDate: Date,
+    readonly payrollStatus: number
+}
+
+export class GeneratePayrollsService {
+
+    public static savePayrolls(payrollsData: Payroll) {
+        const sql = `INSERT INTO "CODBEX_PAYROLLENTRY" ("PAYROLLENTRY_ID","PAYROLLENTRY_EMPLOYEE", "PAYROLLENTRY_TITLE", "PAYROLLENTRY_NETSALARY","PAYROLLENTRY_TAXES","PAYROLLENTRY_STARTDATE","PAYROLLENTRY_ENDDATE","PAYROLLENTRY_PAYROLLSTATUS") values (?, ?, ?, ?, ?,?,?,?)`;
+        const queryParameters = [payrollsData.id, payrollsData.employee, payrollsData.title, payrollsData.netSalary, payrollsData.taxes, payrollsData.startDate, payrollsData.endDate, payrollsData.payrollStatus];
+        insert.execute(sql, queryParameters);
+    }
+
+}
