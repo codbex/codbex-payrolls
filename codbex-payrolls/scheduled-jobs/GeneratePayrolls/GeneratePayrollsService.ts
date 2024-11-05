ccbaca5a4a49e669ae7d408c406ae60d075ee2ad
NicoleNG18 <nicole.georgieva18@abv.bg>
2024-11-05 16:13:41 +0200

    change scheduled job to work with new edm structure

--- codbex-payrolls/scheduled-jobs/GeneratePayrolls/GeneratePayrollsService.ts
+++ codbex-payrolls/scheduled-jobs/GeneratePayrolls/GeneratePayrollsService.ts
@@ -4,18 +4,30 @@ export interface Payroll {
     readonly id: number,
     readonly employee: number,
     readonly title: string,
-    readonly netSalary: number,
-    readonly taxes: number,
+    readonly amount: number,
     readonly startDate: Date,
     readonly endDate: Date,
     readonly payrollStatus: number
 }
 
+export interface PayrollEntryItem {
+    readonly id: number;
+    readonly itemType: number;
+    readonly amount: number;
+    readonly payrollEntry: number;
+}
+
 export class GeneratePayrollsService {
 
-    public static savePayrolls(payrollsData: Payroll) {
-        const sql = `INSERT INTO "CODBEX_PAYROLLENTRY" ("PAYROLLENTRY_ID","PAYROLLENTRY_EMPLOYEE", "PAYROLLENTRY_TITLE", "PAYROLLENTRY_NETSALARY","PAYROLLENTRY_TAXES","PAYROLLENTRY_STARTDATE","PAYROLLENTRY_ENDDATE","PAYROLLENTRY_STATUS") values (?, ?, ?, ?, ?,?,?,?)`;
-        const queryParameters = [payrollsData.id, payrollsData.employee, payrollsData.title, payrollsData.netSalary, payrollsData.taxes, payrollsData.startDate, payrollsData.endDate, payrollsData.payrollStatus];
+    public static savePayroll(payrollsData: Payroll) {
+        const sql = `INSERT INTO "CODBEX_PAYROLLENTRY" ("PAYROLLENTRY_ID","PAYROLLENTRY_EMPLOYEE", "PAYROLLENTRY_TITLE","PAYROLLENTRY_AMOUNT","PAYROLLENTRY_STARTDATE","PAYROLLENTRY_ENDDATE","PAYROLLENTRY_STATUS") values (?, ?, ?, ?, ?,?,?)`;
+        const queryParameters = [payrollsData.id, payrollsData.employee, payrollsData.title, payrollsData.amount, payrollsData.startDate, payrollsData.endDate, payrollsData.payrollStatus];
+        insert.execute(sql, queryParameters);
+    }
+
+    public static savePayrollEntryItem(payrollsData: PayrollEntryItem) {
+        const sql = `INSERT INTO "CODBEX_PAYROLLENTRYITEM" ("PAYROLLENTRYITEM_ID","PAYROLLENTRYITEM_TYPE", "PAYROLLENTRYITEM_AMOUNT", "PAYROLLENTRYITEM_PAYROLLENTRY") values (?, ?, ?, ?)`;
+        const queryParameters = [payrollsData.id, payrollsData.itemType, payrollsData.amount, payrollsData.payrollEntry];
         insert.execute(sql, queryParameters);
     }
 
--- codbex-payrolls/scheduled-jobs/GeneratePayrolls/generate-payrolls-handler.mjs
+++ codbex-payrolls/scheduled-jobs/GeneratePayrolls/generate-payrolls-handler.mjs
@@ -2,26 +2,20 @@ import { GeneratePayrollsService } from './GeneratePayrollsService';
 
 import { EmployeeRepository } from "codbex-employees/gen/codbex-employees/dao/Employees/EmployeeRepository";
 import { SalaryRepository } from "codbex-salaries/gen/codbex-salaries/dao/Salaries/SalaryRepository";
+import { SalaryItemRepository } from "codbex-salaries/gen/codbex-salaries/dao/Salaries/SalaryItemRepository";
 import { PayrollEntryRepository } from "codbex-payrolls/gen/codbex-payrolls/dao/Payrolls/PayrollEntryRepository";
+import { PayrollEntryItemRepository } from "codbex-payrolls/gen/codbex-payrolls/dao/Payrolls/PayrollEntryItemRepository";
 
 const PayrollDao = new PayrollEntryRepository();
 const EmployeeDao = new EmployeeRepository();
 const SalaryDao = new SalaryRepository();
+const SalaryItemDao = new SalaryItemRepository();
+const PayrollEntryItemDao = new PayrollEntryItemRepository();
 
 const employees = EmployeeDao.findAll();
+const payrollsCount = PayrollDao.count();
 
 employees.forEach((employee) => {
-
-    const salary = SalaryDao.findAll({
-        $filter: {
-            equals: {
-                Employee: employee.Id
-            }
-        }
-    });
-
-    const payrollTaxes = salary[0].Gross - salary[0].Net;
-    const payrollsCount = PayrollDao.count();
 
     const date = new Date();
     date.setMonth(date.getMonth() + 1);
@@ -37,12 +31,42 @@ employees.forEach((employee) => {
         "id": payrollsCount + 1,
         "employee": employee.Id,
         "title": payrollTitle,
-        "netSalary": salary[0].Net,
-        "taxes": payrollTaxes,
+        "amount": 0,
         "startDate": firstDayOfMonth.toLocaleDateString('en-CA'),
         "endDate": lastDayOfMonth.toLocaleDateString('en-CA'),
         "payrollStatus": 2
     };
 
-    GeneratePayrollsService.savePayrolls(payroll);
+    GeneratePayrollsService.savePayroll(payroll);
+
+    const salary = SalaryDao.findAll({
+        $filter: {
+            equals: {
+                Employee: employee.Id
+            }
+        }
+    });
+
+    const salaryItems = SalaryItemDao.findAll({
+        $filter: {
+            equals: {
+                Salary: salary[0].Id
+            }
+        }
+    });
+
+    salaryItems.forEach((item) => {
+
+        const payrollsEntryItemsCount = PayrollEntryItemDao.count();
+
+        const payrollItem = {
+            "id": payrollsEntryItemsCount + 1,
+            "itemType": item.Type,
+            "amount": item.Quantity,
+            "payrollEntry": payrollsCount + 1
+        };
+
+        GeneratePayrollsService.savePayrollEntryItem(payrollItem);
+    });
+
 });
