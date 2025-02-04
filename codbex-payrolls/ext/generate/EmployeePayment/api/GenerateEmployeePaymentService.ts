5f416f67fddbc0ae110036e2eb982e4ad092d976
NicoleNG18 <nicole.georgieva18@abv.bg>
2025-02-04 18:37:33 +0200

    move extension to the module

---
+++ codbex-payrolls/ext/generate/EmployeePayment/api/GenerateEmployeePaymentService.ts
@@ -0,0 +1,76 @@
+import { Controller, Get } from "sdk/http";
+
+import { PayrollEntryRepository as PayrollEntryDao } from "codbex-payrolls/gen/codbex-payrolls/dao/Payrolls/PayrollEntryRepository";
+import { EmployeeRepository as EmployeeDao } from "codbex-employees/gen/codbex-employees/dao/Employees/EmployeeRepository";
+import { SalaryRepository as SalaryDao } from "codbex-salaries/gen/codbex-salaries/dao/Salaries/SalaryRepository";
+import { CurrencyRepository as CurrencyDao } from "codbex-currencies/gen/codbex-currencies/dao/Currencies/CurrencyRepository";
+
+@Controller
+class GenerateEmployeePaymentService {
+
+    private readonly payrollEntryDao;
+    private readonly employeeDao;
+    private readonly salaryDao;
+    private readonly currencyDao;
+
+    constructor() {
+        this.payrollEntryDao = new PayrollEntryDao();
+        this.employeeDao = new EmployeeDao();
+        this.salaryDao = new SalaryDao();
+        this.currencyDao = new CurrencyDao();
+    }
+
+    @Get("/payrollData/:payrollId")
+    public payrollData(_: any, ctx: any) {
+        const payrollId = ctx.pathParameters.payrollId;
+
+        const payrollEntry = this.payrollEntryDao.findById(payrollId);
+
+        return {
+            "Id": payrollEntry.Id,
+            "Employee": payrollEntry.Employee,
+            "Title": payrollEntry.Title,
+            "Status": payrollEntry.Status,
+            "StartDate": payrollEntry.StartDate,
+            "EndDate": payrollEntry.EndDate,
+            "PayDate": payrollEntry.PayDate
+        };
+    }
+
+    @Get("/employeeData/:employeeId")
+    public employeeData(_: any, ctx: any) {
+        const employeeId = ctx.pathParameters.employeeId;
+
+        const employeeEntry = this.employeeDao.findById(employeeId);
+
+        return {
+            "Name": employeeEntry.Name,
+            "IBAN": employeeEntry.IBAN
+        };
+    }
+
+    @Get("/salaryData/:salaryId")
+    public salaryData(_: any, ctx: any) {
+        const salaryId = ctx.pathParameters.salaryId;
+
+        const salaryEntry = this.salaryDao.findById(salaryId);
+
+        const currency = this.currencyDao.findAll({
+            $filter: {
+                equals: {
+                    Id: salaryEntry.Currency
+                }
+            }
+        });
+
+        const taxes = salaryEntry.Gross - salaryEntry.Net;
+
+        return {
+            "Currency": salaryEntry.Currency,
+            "CurrencyCode": currency[0].Code,
+            "Net": salaryEntry.Net,
+            "Taxes": taxes
+        };
+    }
+
+}
---
+++ codbex-payrolls/ext/generate/EmployeePayment/employee-payment-generate-controller.js
@@ -0,0 +1,83 @@
+const app = angular.module('templateApp', ['ideUI', 'ideView']);
+
+app.controller('templateController', ['$scope', '$http', 'ViewParameters', 'messageHub', function ($scope, $http, ViewParameters, messageHub) {
+    const params = ViewParameters.get();
+    $scope.showDialog = true;
+
+    const employeeUrl = "/services/ts/codbex-payrolls/ext/generate/EmployeePayment/api/GenerateEmployeePaymentService.ts/employeeData/";
+    const payrollEntryUrl = "/services/ts/codbex-payrolls/ext/generate/EmployeePayment/api/GenerateEmployeePaymentService.ts/payrollData/" + params.id;
+    const salaryUrl = "/services/ts/codbex-payrolls/ext/generate/EmployeePayment/api/GenerateEmployeePaymentService.ts/salaryData/";
+    const employeePaymentUrl = "/services/ts/codbex-payments/gen/codbex-payments/api/EmployeePayment/EmployeePaymentService.ts/";
+    const payrollUpdateUrl = "/services/ts/codbex-payrolls/gen/codbex-payrolls/api/Payrolls/PayrollEntryService.ts/";
+
+    $http.get(payrollEntryUrl)
+        .then(function (response) {
+
+            $scope.PayrollData = response.data;
+
+            if (response.data.Status == 1) {
+                $scope.Paid = true;
+            } else {
+                $scope.Paid = false;
+            }
+
+            const date = new Date(response.data.StartDate);
+
+            $scope.MonthName = date.toLocaleString('default', { month: 'long' });
+            $scope.Year = date.getFullYear();
+
+            return $http.get(employeeUrl + response.data.Employee);
+        })
+        .then(function (response) {
+            $scope.Employee = response.data;
+
+            return $http.get(salaryUrl + $scope.PayrollData.Employee);
+        })
+        .then(function (response) {
+            console.log(response.data);
+            $scope.SalaryData = response.data;
+        });
+
+    $scope.paySalary = function () {
+
+        const employeePayment = {
+            "Date": new Date().toLocaleDateString('en-CA'),
+            "Valor": new Date().toLocaleDateString('en-CA'),
+            "CounterpartyIBAN": $scope.Employee.IBAN,
+            "CounterpartyName": $scope.Employee.Name,
+            "Amount": $scope.SalaryData.Net,
+            "Currency": $scope.SalaryData.Currency,
+            "Reason": $scope.PayrollData.Title
+        }
+
+        $http.post(employeePaymentUrl, employeePayment)
+            .then(function (response) {
+
+                $scope.PayrollData.PayDate = new Date().getTime();
+                $scope.PayrollData.PayrollStatus = 1;
+
+                $http.put(payrollUpdateUrl + $scope.PayrollData.Id, $scope.PayrollData)
+                    .then(function (response) {
+                        console.log(response.data);
+                        $scope.closeDialog();
+                    })
+                    .catch(function (error) {
+                        console.error("Error updating Payroll entry", error);
+                    });;
+
+            })
+            .catch(function (error) {
+                console.error("Error creating Employee payment", error);
+            });
+        ;
+
+    };
+
+    $scope.closeDialog = function () {
+        $scope.showDialog = false;
+        messageHub.closeDialogWindow("employee-payment-generate");
+    };
+
+    document.getElementById("dialog").style.display = "block";
+}]);
+
